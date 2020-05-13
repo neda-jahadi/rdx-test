a8e9560ff17278dc6114ce1bdea764b815e8e7df
Neda Jahadi <neda_jahadi@yahoo.com>
2020-05-13 22:01:35 +0200

    Updates

---
+++ precache-manifest.6ca8928d39b8f367fd64172d54c834fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6381ad7e6ff9d6b1046d28f59700e3d8",
+    "url": "/rdx-test/index.html"
+  },
+  {
+    "revision": "313e1ba3db1343ff7aaa",
+    "url": "/rdx-test/static/css/main.07e83b60.chunk.css"
+  },
+  {
+    "revision": "1bca70b6e32db847683f",
+    "url": "/rdx-test/static/js/2.805c2785.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rdx-test/static/js/2.805c2785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313e1ba3db1343ff7aaa",
+    "url": "/rdx-test/static/js/main.65454676.chunk.js"
+  },
+  {
+    "revision": "bf5ca62e09d7e70851ec",
+    "url": "/rdx-test/static/js/runtime-main.235454b2.js"
+  }
+]);
